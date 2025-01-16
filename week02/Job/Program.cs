762e3f29578b633356fcf247530084c9505ab9b2
Gabriel Alba <gabriel.gap60@gmail.com>
2025-01-16 19:31:44 -0400

    code

--- week02/Job/Program.cs
+++ week02/Job/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Job
+{
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
